0cbc5c245e4366b0bb060c13d55c2dc1600da934
kim <ing03201@gmail.com>
2021-06-21 22:40:34 +0900

    add shell

---
+++ run.sh
@@ -0,0 +1,25 @@
+chmod 775 /sys/class/gpio/export
+chmod 775 /sys/class/gpio/unexport
+for gpio in "65" "68" "70" "71" "72" "73" "74" "76"; do
+    GPIO_DIR=$(ls /sys/class/gpio/ | grep ${gpio})
+    if [ -n "$GPIO_DIR" ]; then
+            echo "GPIO already exist from past error"
+            echo ${gpio} > /sys/class/gpio/unexport
+            sleep 1
+    fi
+    echo ${gpio} > /sys/class/gpio/export
+    sleep 1
+    echo "out" > /sys/class/gpio/gpio${gpio}/direction
+done
+
+for gpio in "111" "112" "113" "114" "117" "118" "75"; do
+    GPIO_DIR=$(ls /sys/class/gpio/ | grep ${gpio})
+    if [ -n "$GPIO_DIR" ]; then
+            echo "GPIO already exist from past error"
+            echo ${gpio} > /sys/class/gpio/unexport
+            sleep 1
+    fi
+    echo ${gpio} > /sys/class/gpio/export
+    sleep 1
+    echo "in" > /sys/class/gpio/gpio${gpio}/direction
+done
