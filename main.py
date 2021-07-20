acec568cb6fd41d6cf74760a087ea14accdbfbf9
kim <ing03201@gmail.com>
2021-07-20 22:58:02 +0900

    can text to speech

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 import paho.mqtt.client as mqtt
-
+from gtts import gTTS
+from playsound import playsound
 #-*- coding:utf-8 -*-
 import paho.mqtt.client as mqtt
 
@@ -14,14 +15,17 @@ def on_connect(client, userdata, flags, rc):
 
 #서버로부터 publish message를 받을 때 호출되는 콜백
 def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload)) #토픽과 메세지를 출력한다.
-
+	recvData = str(msg.payload.decode("utf-8"))
+	print(recvData) #토픽과 메세지를 출력한다.
+	tts = gTTS(text=recvData, lang="ko", slow=False)
+	tts.save("helloEN.mp3")
+	playsound("hellowEN.mp3")
 
 client.on_connect = on_connect #콜백설정
 client.on_message = on_message #콜백설정
 
 
 if __name__ == '__main__':
-	broker_server = input("서버 IP주소를 입력해주세요")
+	broker_server = input("서버 IP주소를 입력해주세요: ")
 	client.connect(broker_server, 1883, 60) #라즈베리파이3 MQTT 브로커에 연결
 	client.loop_forever()
