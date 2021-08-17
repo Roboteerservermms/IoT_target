b90d2ef3fcbff58fc93ba0c3ffdc26e90386f71e
kim <ing03201@gmail.com>
2021-08-17 17:02:24 +0900

    change tcp to udp

--- server.py
+++ server.py
@@ -5,8 +5,9 @@ from gtts import gTTS
 from video import VlcPlayer
 import time
 import pafy
-import urllib.request
+import requests
 from vlc import EventType
+
 
 GPIOOUT = [111, 112, 113, 114, 117, 118, 75]
 GPIOIN = [65, 68, 70, 71, 72, 73, 74, 76]
@@ -47,12 +48,12 @@ def gpio_handler(event):
             break
     player.play(video_dic[None])
         
-def json_protocol(recv_data):
-    recv_data.decode("utf-8")
-    command = json.loads(recv_data)
-    print(command)
+def json_protocol(msg):
+    command = json.loads(msg)
     video_dic[ command["GPIO_IN"] ]  = command["data"]
+    print(video_dic)
     out_dic[ command["GPIO_IN"] ] = command["GPIO_OUT"]
+    print(out_dic)
 
 def quit_server(client_addr): 
     print("{} was gone".format(client_addr))
@@ -60,17 +61,14 @@ def quit_server(client_addr):
 
 # -*- coding: utf-8 -*- 
 import socketserver
-class MyTCPHandler(socketserver.BaseRequestHandler):
+class MyUDPHandler(socketserver.BaseRequestHandler):
     """ The request handler class for our server. It is instantiated once per connection to the server, and must override the handle() method to implement communication to the client. """
     def handle(self): 
         """ 클라이언트와 연결될 때 호출되는 함수 상위 클래스에는 handle() 메서드가 정의되어 있지 않기 때문에 여기서 오버라이딩을 해야함 """
-        self.recv_data = ''
-        while True:
-            try:
-                recv_data = self.request.recv(1024)
-                json_protocol(recv_data)
-            except NameError as e:
-                print( f"{self.client_address[0]} got an error : {e}")
+        data = self.request[0]
+        socket = self.request[1]
+        print(f"{self.client_address[0]} wrote: {data}")
+        json_protocol(data.decode("utf-8"))
 
 if __name__ == "__main__": 
     HOST, PORT = "0.0.0.0", 8080 
@@ -78,7 +76,7 @@ if __name__ == "__main__":
     player = VlcPlayer('--input-repeat=999999', '--mouse-hide-timeout=0')
     player.play(video_dic[None])
     player.add_callback(EventType.MediaPlayerEndReached,gpio_handler)
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socketserver.UDPServer((HOST, PORT), MyUDPHandler)
     print("waiting for connection...") 
     # Ctrl - C 로 종료하기 전까지는 서버는 멈추지 않고 작동 
     server.serve_forever()
