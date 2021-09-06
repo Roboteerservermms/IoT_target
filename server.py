88f3f5e676fab35f11375482d34aa750ff484580
kim <ing03201@gmail.com>
2021-09-06 15:55:36 +0900

    add

--- server.py
+++ server.py
@@ -37,21 +37,20 @@ def sig_handler(signum, frame):
     global exitThread
     exitThread = True
 
-def TTS(GPIOIN, data):
+def TTS(IN, data):
     nowTime = time.strftime("%Y%m%d-%H%M%S")
     tts = gTTS(text=data, lang="ko", slow=False)
     fileName=f"{nowTime}.mp3"
     tts.save(fileName)
-    video_dic[GPIOIN] = fileName
-    
+    video_dic[IN] = fileName
 
-def rtsp(GPIOIN, data):
+def rtsp(IN, data):
     video = pafy.new(data)
     best = video.getbest()
-    video_dic[GPIOIN] = best.url
+    video_dic[IN] = best.url
 
-def broadcast(GPIOIN, fileName):
-    video_dic[GPIOIN] = fileName
+def broadcast(IN, fileName):
+    video_dic[IN] = fileName
 
 def video_end_handler(event):
     logger.info("video end reached!")
@@ -63,53 +62,10 @@ def scheduler_sig_handler():
     global schedule_sig
     schedule_sig = True
 
-def scheduler(day,data,time=None):
-    if day == "mon":
-        schedule.every().monday.at(f"{time}").do(scheduler_sig_handler)
-    elif day == "tue":
-        schedule.every().tuesday.at(f"{time}").do(scheduler_sig_handler)
-    elif day == "wen":
-        schedule.every().wednesday.at(f"{time}").do(scheduler_sig_handler)
-    elif day == "thu":
-        schedule.every().thursday.at(f"{time}").do(scheduler_sig_handler)
-    elif day == "fri":
-        schedule.every().friday.at(f"{time}").do(scheduler_sig_handler)
-    elif day == "sat":
-        schedule.every().saturday.at(f"{time}").do(scheduler_sig_handler)
-    elif day == "sun":
-        schedule.every().sunday.at(f"{time}").do(scheduler_sig_handler)
-    elif day == "everyday":
-        schedule.every().day.at(f"{time}").do(scheduler_sig_handler)
-    else:
-        schedule.every().day.at(f"{time}").do(scheduler_sig_handler)
-    schedule_dic[f"{time}"] = data
-    print(f"{schedule_dic}")
-
-def json_protocol(msg):
-    global command
-    command = json.loads(msg)
-    if command["category"] == "schedule":
-        try :
-            scheduler(command["day"], command["data"],  command["time"])
-        except KeyError as e:
-            scheduler(command["day"], command["data"])
-    elif command["category"] == "TTS":
-        TTS(command["GPIO_IN"],command["data"])
-        out_dic[ command["GPIO_IN"] ] = command["GPIO_OUT"]
-    elif command["category"] == "rtsp":
-        rtsp(command["GPIO_IN"],command["data"])
-        out_dic[ command["GPIO_IN"] ] = command["GPIO_OUT"]
-    else:
-        broadcast(command["GPIO_IN"],command["data"])
-        out_dic[ command["GPIO_IN"] ] = command["GPIO_OUT"]
-    logger.info(video_dic)
-    
-    logger.info(out_dic)
-
-def quit_server(client_addr): 
+def quit_server(client_addr):
     logger.info("{} was gone".format(client_addr))
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     HOST, PORT, bufferSize = "0.0.0.0", 8080 , 1024
     # 서버를 생성합니다. 호스트는 localhost, 포트 번호는 8080
     player = VlcPlayer('--mouse-hide-timeout=0')
@@ -125,7 +81,7 @@ if __name__ == "__main__":
         if schedule_sig:
             now_time = time.strftime('%H:%M')
             logger.info("schedule is running!")
-            player.play(schedule_dic[now_time])
+            player.play(scheduleList[now_time])
         elif video_sig:
             video_sig = False
             index = None
@@ -137,6 +93,7 @@ if __name__ == "__main__":
                     out_command = f"echo 1 > /sys/class/gpio/gpio{out_dic[i]}/value"
                 subprocess.getoutput(out_command)
             player.play(video_dic[index])
+            logger.info(f"in {index} out {out_dic[index]} video {video_dic[index]}")
         try:
             recvdata, addr = UDPServerSocket.recvfrom(bufferSize) 
             data = recvdata.decode("utf-8")
@@ -175,8 +132,8 @@ if __name__ == "__main__":
                 rtsp(command["GPIO_IN"],command["data"])
             else:
                 broadcast(command["GPIO_IN"],command["data"])
-            logger.info(videoList)
-            UDPServerSocket.sendto(json.dumps(videoList).encode(), address)
+            logger.info(video_dic)
+            logger.info(out_dic)
         except socket.error:
             pass
         # Ctrl - C 로 종료하기 전까지는 서버는 멈추지 않고 작동
