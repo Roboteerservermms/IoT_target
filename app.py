502db2d1ce65916eb81c85df2b8ed5d5745fc743
kim <ing03201@gmail.com>
2021-09-23 10:31:21 +0900

    Auto stash before merge of "test" and "udp"

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
- 
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-import os, subprocess, json
+import os, subprocess, json, requests
 from flask_restx import Resource, Api
-from API.Web import WebApi
 from API.Broadcast import BroadcastApi
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #파일 업로드 용량 제한 단위:바이트
 #HTML 렌더링
 api = Api(
@@ -17,13 +15,25 @@ api = Api(
     contact="ing03201@gmail.com",
     license="MIT"
 )
+jsonPath = "./json/"
 
-api.add_namespace(WebApi, '/Web')
-api.add_namespace(BroadcastApi, '/Broadcast')
+@app.route('/')
+def home_page():
+    return render_template('home.html')
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+@app.route('/<ip>')
+def device_page(ip):
+
+    res = requests.post(f"http://{ip}/getMacAddress")
+    with open(f"{jsonPath}{ip}.json") as f:
+        jsonData = json.load(f)
+    try :
+        res.raise_for_status()
+    except:
+        return None
+
+    return 
+
 
 #서버 실행
 if __name__ == '__main__':
